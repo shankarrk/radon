582351291ea92adb1d36fd45058bfa83cc3c5007
shankarrk <sangaranna@hotmail.com>
2015-07-08 22:21:42 -0700

    basics
    adding jsconfig for ES 6  and adding index.html

---
+++ src/js/radon.js
@@ -0,0 +1,29 @@
+var radon = !function () {
+	const radon = { version: '0.0.1' };
+
+	//setup the application
+	function setup(rootCanvas = null) {
+		if (rootCanvas) {
+			drawScales(rootCanvas);
+		}
+	}
+
+
+	//draw the scales
+	function drawScales(rootCanvas = null) {
+		if (rootCanvas) {
+			
+		}
+	}
+	
+	//Draw the basic canvas surface
+	function drawSurface(rootCanvas = null) {
+		if(rootCanvas)
+		{
+			
+		}
+	}
+
+	this.radon = radon;
+	return this;
+} ();
